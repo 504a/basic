d85426fed0870f625d6f23e60953383759a80718
504a <moritaka@wdc-kd.com>
2021-04-28 13:16:17 +0900

    20210428

---
+++ src/ex/ex12/Ex12.java
@@ -0,0 +1,30 @@
+package ex.ex12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+//名前の完全一致で検索する
+class Ex12 {
+    public static void main(String[] args) {
+        List<Person> list = new ArrayList<>();
+        list.addAll(Arrays.asList(
+                new Person("あいざわ","男",25),
+                new Person("おおた","女",18),
+                new Person("えんどう","男",39),
+                new Person("うえの","女",33),
+                new Person("いのうえ","男",42)
+        ));
+
+        //一覧表示
+        System.out.println("一覧表示");
+        for (Person p :list) System.out.println(p);
+
+        //検索する名前の入力
+        Scanner in = new Scanner(System.in);
+        System.out.println("検索する名前を入力");
+        String name = in.nextLine();
+        System.out.println(name);
+    }
+}
