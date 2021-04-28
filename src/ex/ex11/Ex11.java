e3097c8f444ff73a70bb02f865c303e7e56f59a3
504a <moritaka@wdc-kd.com>
2021-04-28 10:14:52 +0900

    20210427

---
+++ src/ex/ex11/Ex11.java
@@ -0,0 +1,12 @@
+package ex.ex11;
+
+class Ex11 {
+    public static void main(String[] args) {
+        //Personクラスを格納するArrayListを作成する
+        //5人程度のインスタンスを追加する
+
+        //一覧表示
+
+        //年齢の降順表示
+    }
+}
---
+++ src/ex/ex11/Person.java
@@ -0,0 +1,9 @@
+package ex.ex11;
+
+class Person {
+    private String name;
+    private String gender;
+    private int age;
+
+
+}
