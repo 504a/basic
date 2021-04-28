37231b57b5f533901c8a75af5976cacab4dab14b
504a <moritaka@wdc-kd.com>
2021-04-28 12:38:58 +0900

    20210428

--- src/objectSample/updateSample/UpdatePerson.java
+++ src/objectSample/updateSample/UpdatePerson.java
@@ -3,7 +3,6 @@ package objectSample.updateSample;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 
 class UpdatePerson {
