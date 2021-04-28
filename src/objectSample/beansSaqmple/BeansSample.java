cc12cb11b46cb0dad4f5218d0cfc423c20ecc71f
504a <moritaka@wdc-kd.com>
2021-04-28 10:05:18 +0900

    20210427

---
+++ src/objectSample/beansSaqmple/BeansSample.java
@@ -0,0 +1,31 @@
+package objectSample.beansSaqmple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Beansの活用
+class BeansSample {
+    public static void main(String[] args) {
+        List<StudentBean> list = new ArrayList<>();
+        //各項目のデータを用意する
+        String name = "やまだ";
+        int grade = 1;
+        int cl = 1;
+        int no = 20;
+        int jap = 80;
+        int math = 70;
+        int eng = 60;
+
+        //Beansに追加する
+        StudentBean studentBean = new StudentBean();//空のインスタンスを作成
+        studentBean.setName(name);
+        studentBean.setGrade(grade);
+        studentBean.setCl(cl);
+        studentBean.setNo(no);
+        studentBean.setJap(jap);
+        studentBean.setMath(math);
+        studentBean.setEng(eng);
+        list.add(studentBean);
+
+    }
+}
