034b59decc55ebeb2bc03ac77eb0032f3935050a
504a <moritaka@wdc-kd.com>
2021-04-28 10:27:14 +0900

    20210428

--- src/ex/ex11/Person.java
+++ src/ex/ex11/Person.java
@@ -5,5 +5,27 @@ class Person {
     private String gender;
     private int age;
 
+    public Person(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %d",
+                name,gender,age);
+    }
 }
