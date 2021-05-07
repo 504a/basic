35507487cef0dc696f40520ef09dba5761adc265
504a <moritaka@wdc-kd.com>
2021-05-07 10:44:52 +0900

    20210507

--- src/objectSample/setSample/Set1.java
+++ src/objectSample/setSample/Set1.java
@@ -1,7 +1,9 @@
 package objectSample.setSample;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 //Setサンプル
@@ -42,5 +44,29 @@ class Set1 {
         System.out.println(set);
         System.out.println(set.removeAll(Arrays.asList(6,7)));//false
         System.out.println(set);
+        //合致しない値を全て削除する
+        System.out.println(set.retainAll(Arrays.asList(1, 2, 3)));//true
+        System.out.println(set);
+        //値を全て削除
+        set.clear();
+        System.out.println(set);
+        //サイズの取得
+        System.out.println(set.size());//0
+        //空であるか？
+        System.out.println(set.isEmpty());//true
+        set.add(1);
+        System.out.println(set.isEmpty());//false
+        //ArrayListからSet変換
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,2,2,3,3,3));
+        System.out.println(list);//1,2,2,3,3,3
+        Set<Integer> integerSet = new HashSet<>(list);
+        System.out.println(integerSet);//1,2,3
+        //値の存在を判定する
+        System.out.println(integerSet.contains(2));//true
+        System.out.println(integerSet.contains(4));//false
+        System.out.println(integerSet.containsAll(Arrays.asList(1, 2, 3)));
+        System.out.println(integerSet.containsAll(Arrays.asList(1, 2, 4)));
+
     }
 }
