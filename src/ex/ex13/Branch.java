93ef83214376038d4ee66cd5b1ab61e0a096f6ce
504a <moritaka@wdc-kd.com>
2021-05-10 14:21:05 +0900

    20210510

---
+++ src/ex/ex13/Branch.java
@@ -0,0 +1,25 @@
+package ex.ex13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Branch {
+    private String name;
+    private List<Employee> employees = new ArrayList<>();
+
+    {
+        employees.add(new Employee(1,"やまだ","男",35));
+        employees.add(new Employee(2,"たけだ","女",22));
+        employees.add(new Employee(3,"はまだ","男",42));
+
+        initSales(employees.get(0),100);
+        initSales(employees.get(1),300);
+        initSales(employees.get(2),200);
+    }
+
+    public static void initSales(Employee employee,int value) {
+        for (int i = 0; i < 12; i++) {
+            employee.setSales(i,value);
+        }
+    }
+}
