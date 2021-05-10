0cd166bb2b3e4abfc4a802102b6327c8bc0043bd
504a <moritaka@wdc-kd.com>
2021-05-10 15:10:47 +0900

    20210510

--- src/ex/ex13/Ex131.java
+++ src/ex/ex13/Ex131.java
@@ -1,5 +1,6 @@
 package ex.ex13;
 
+import java.util.Comparator;
 import java.util.List;
 
 class Ex131 {
@@ -12,6 +13,22 @@ class Ex131 {
                     employee + " " + employee.getAllSales() +
                     " " + employee.getSum() +
                     " " + employee.getAve());
+        }
+
+        //支店合計の表示
+        int sum = 0;
+        for (Employee employee:employees) {
+            sum+=employee.getSum();
+        }
+        System.out.println("支店合計の表示");
+        System.out.println(branch.getName() + ":" + sum);
+
+        //支店の上位３名の表示
+        System.out.println("支店売上TOP3");
+        employees.sort(Comparator.comparing(Employee::getSum,Comparator.reverseOrder()));
+        for (int i = 0; i < 3; i++) {
+            System.out.println(employees.get(i).getName() + ":" +
+                    employees.get(i).getSum());
         }
     }
 }
