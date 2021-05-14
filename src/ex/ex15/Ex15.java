cee39376bbb4650e823e92dcf6b1eff81e83d99f
504a <moritaka@wdc-kd.com>
2021-05-14 13:31:25 +0900

    20210514

---
+++ src/ex/ex15/Ex15.java
@@ -0,0 +1,11 @@
+package ex.ex15;
+
+class Ex15 {
+    public static void main(String[] args) {
+        /*
+            Personクラスをフィールドnoで検索出来る処理をMapを使って作成しなさい。
+            検索するメソッドsearchPersonを作成してOptional<Person>で戻り値を作成するメソッドを作成すること。
+
+         */
+    }
+}
