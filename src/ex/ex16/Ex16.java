b722dcc5b35c6d3a5dcee62e65212f4dd68a75e2
504a <moritaka@wdc-kd.com>
2021-05-18 10:49:54 +0900

    20210518

---
+++ src/ex/ex16/Ex16.java
@@ -0,0 +1,10 @@
+package ex.ex16;
+
+import java.util.List;
+
+class Ex16 {
+    public static void main(String[] args) {
+        List<String> zipCode = List.of("000-0000","1234-567","A23-b567","123-4567");
+        List<String> phoneNo = List.of("03-1234-5678","011-234-5678","0123-12-9999","123-456-789");
+    }
+}
