9a43de26f8f756a80ed5da3cf7e35d792da178ef
504a <moritaka@wdc-kd.com>
2021-05-31 14:27:36 +0900

    20210531

--- src/ex/ex21/Ex21.java
+++ src/ex/ex21/Ex21.java
@@ -1,11 +1,21 @@
 package ex.ex21;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class Ex21 {
     public static void main(String[] args) {
         //1から100までの整数型ArrayListを作成する
+        List<Integer> integerList = IntStream.rangeClosed(1,100).boxed()
+                .collect(Collectors.toList());
 
-        //偶数のみを処理する
-        //対象の数値を3倍する
-        //数値の大きいものから表示する
+
+        integerList.stream()
+                .filter(i -> i % 2 == 0)//偶数のみを処理する
+                .map(i -> i * 3)//対象の数値を3倍する
+                .sorted(Comparator.reverseOrder())//数値の大きいものから表示する
+                .forEach(System.out::println);
     }
 }
