4720cf5368bab951189c8a01eb3a30bab04d70a3
504a <moritaka@wdc-kd.com>
2021-05-31 13:50:49 +0900

    20210531

---
+++ src/ex/ex21/Ex21.java
@@ -0,0 +1,11 @@
+package ex.ex21;
+
+class Ex21 {
+    public static void main(String[] args) {
+        //1から100までの整数型ArrayListを作成する
+
+        //偶数のみを処理する
+        //対象の数値を3倍する
+        //数値の大きいものから表示する
+    }
+}
