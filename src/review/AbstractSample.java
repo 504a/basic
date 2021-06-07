7d9fd33ad318a7ef0e648c4848d00cc6a3f4248c
504a <moritaka@wdc-kd.com>
2021-06-07 13:49:29 +0900

    20210607

---
+++ src/review/AbstractSample.java
@@ -0,0 +1,8 @@
+package review;
+
+class AbstractSample {
+    public static void main(String[] args) {
+        AbstractTest at = new TestC("やまだ");
+        System.out.println(at.getName());
+    }
+}
