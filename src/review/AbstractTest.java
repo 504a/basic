6634ed1099a3a2d481cc0f6b0a22cd57188fe40e
504a <moritaka@wdc-kd.com>
2021-06-07 13:34:05 +0900

    20210607

---
+++ src/review/AbstractTest.java
@@ -0,0 +1,11 @@
+package review;
+
+abstract class AbstractTest {
+    protected String name;
+
+    public AbstractTest(String name) {
+        this.name = name;
+    }
+
+    public abstract String getName();
+}
