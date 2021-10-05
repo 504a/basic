71caea2d2133e57764337cb8c0878ba0c65f84cb
504a <moritaka@wdc-kd.com>
2021-10-05 10:39:02 +0900

    20211005

--- src/ex/Rps.java
+++ src/ex/Rps.java
@@ -10,6 +10,7 @@ public class Rps {
         //乱数の発生
         Random random = new Random(LocalDateTime.now().getSecond());
         int cpu = random.nextInt(3);//0,1,2の三種類の値が発生する
+        //int cpu = 2;
 
         //プレイヤーの手の入力
         Scanner scanner = new Scanner(System.in);
@@ -44,5 +45,14 @@ public class Rps {
         }
 
         //勝敗の結果表示
+        if(cpu == player) {
+            System.out.println("あいこです");
+        } else if (cpu == 1 && player == 0//cpu チョキ player グー
+            || cpu == 2 && player ==1//cpu パー player チョキ
+            || cpu == 0 && player == 2) {//cpu グー player パー
+            System.out.println("あなたの勝ちです");
+        } else {
+            System.out.println("あなたの負けです");
+        }
     }
 }
