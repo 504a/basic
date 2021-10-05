92c0ac8f3a6c8015c87caa2f9a9894fc0c71838b
504a <moritaka@wdc-kd.com>
2021-10-05 15:23:42 +0900

    20211005

--- src/intro/order/For1.java
+++ src/intro/order/For1.java
@@ -6,7 +6,7 @@ public class For1 {
         int sum = 0;//合計の初期化
         for (int i = 1; i <= 100 ; i++) {
             sum += i;
-            System.out.println(i + "回目: " + sum);
+            System.out.println(i + "回目:" + sum);
         }
     }
 }
